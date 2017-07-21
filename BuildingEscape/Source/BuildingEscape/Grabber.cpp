8cb78f8258830655b9479217675cac698f870f32
sanctorium <otero_jose@hotmail.com>
2017-07-21 16:46:28 +0200

    87. Refactoring Rules
    Refactoring Physics Handle Code.

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -15,7 +15,6 @@ UGrabber::UGrabber()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
 }
 
 
@@ -30,7 +29,6 @@ void UGrabber::BeginPlay()
 	UE_LOG(LogTemp, Warning, TEXT("%s's grabber reporting for duty!"), *PawnName);
 	*/
 	FindPhysicsHandleComponent();
-
 	SetupInputComponent();
 }
 
@@ -54,8 +52,6 @@ void UGrabber::SetupInputComponent() {
 }
 
 void UGrabber::Grab() {
-	UE_LOG(LogTemp, Warning, TEXT("Grab pressed"))
-
 	/// LINE TRACE and see if we reach any actors with physics body collision channel set
 	auto HitResult = GetFirstPhysicsBodyInReach();
 	auto ActorHit{ HitResult.GetActor() };
@@ -65,8 +61,8 @@ void UGrabber::Grab() {
 	if (ActorHit) {
 		// Attach physics handle
 		PhysicsHandle->GrabComponent(
-			ComponentToGrab,
-			NAME_None,
+			ComponentToGrab,	// Gets the mesh in our case
+			NAME_None,	// No bones needed
 			ComponentToGrab->GetOwner()->GetActorLocation(),
 			true	// Allow rotation
 		);
@@ -75,46 +71,31 @@ void UGrabber::Grab() {
 }
 
 void UGrabber::Release() {
-	UE_LOG(LogTemp, Warning, TEXT("Grab released"))
-
-		// TODO Release physics handle
+		// Release physics handle
 		PhysicsHandle->ReleaseComponent();
 }
 
 // Called every frame
 void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	/// Get player view point
-	FVector PlayerViewPointLocation;
-	FRotator PlayerViewPointRotation;
-	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
-		OUT PlayerViewPointLocation,
-		OUT PlayerViewPointRotation
-	);
-	FVector	LineTraceEnd{ PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach };
+	/*
+	Update the location and reach of the player. We need it to update the location of the current reach (LineTraceEnd)
+	of the player to be able to move the object that we are holding (if any).
+	*/
+	CalculatePlayerViewpointAndReach();
 
 	// if the physics handle is attached
 	if (PhysicsHandle->GrabbedComponent) {
-		// move the object that we're holding
+		// move the object that we're holding. Holding it at the end of the player's reach.
 		PhysicsHandle->SetTargetLocation(LineTraceEnd);
 	}
-		
-
-		
-		
 		
 	/*
 	The code below was enterely refactored into the function "FHitResult GetFirstPhysicsBodyInReach() const"
 	I'm leaving it here to see where code can go.
 
 	/// Get player view point
-	FVector PlayerViewPointLocation;
-	FRotator PlayerViewPointRotation;
-	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
-		OUT PlayerViewPointLocation,
-		OUT PlayerViewPointRotation
-	);
+	CalculatePlayerViewpointAndReach();
 
 	The code below just output the x, y, z coordinates of the position of the player, and the roll, pitch, and yaw
 	of its rotation:
@@ -139,34 +120,32 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 	*/
 }
 
-FHitResult UGrabber::GetFirstPhysicsBodyInReach() const {
-
-	/// Get player view point
-	FVector PlayerViewPointLocation;
-	FRotator PlayerViewPointRotation;
+void UGrabber::CalculatePlayerViewpointAndReach() {
 	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
 		OUT PlayerViewPointLocation,
 		OUT PlayerViewPointRotation
 	);
+	LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
+}
 
-	FVector	LineTraceEnd{ PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach };
-
+FHitResult UGrabber::GetFirstPhysicsBodyInReach() const {
 	/// Line-trace (AKA ray-cast) out to reach distance
 	FCollisionQueryParams TraceParameters{ FName{ TEXT("") }, false, GetOwner() };
-	FHitResult Hit;
+	FHitResult HitResult;
 
 	/// LineTraceMultiBy_____ passes through multiple objects and returns different references.
 	bool Hitted{ GetWorld()->LineTraceSingleByObjectType(
-		OUT Hit,
+		OUT HitResult,
 		PlayerViewPointLocation,
 		LineTraceEnd,
 		FCollisionObjectQueryParams{ ECollisionChannel::ECC_PhysicsBody },
 		TraceParameters
 	) };
+	// Udemy's instructor, at lesson 87, said we don't need to check for this (Hitted) anymore.
+	// I don't know why, so I'm leaving it.
 	if (Hitted) {
-		FString ReachedActor{ Hit.GetActor()->GetName() };
+		FString ReachedActor{ HitResult.GetActor()->GetName() };
 		UE_LOG(LogTemp, Warning, TEXT("%s is at reaching distance!"), *ReachedActor)
 	}
-
-	return Hit;
+	return HitResult;
 }
--- BuildingEscape/Source/BuildingEscape/Grabber.h
+++ BuildingEscape/Source/BuildingEscape/Grabber.h
@@ -29,6 +29,11 @@ private:
 	// Name of the Actor owner of this component.
 	FString PawnName;
 
+	// Player location, rotation, and reach
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+	FVector	LineTraceEnd;	// The end of this line determines the maximum reach for the player
+
 	// How far ahead of the player can we reach in centimeters.
 	float Reach{ 100.f };
 
@@ -39,6 +44,9 @@ private:
 	void FindPhysicsHandleComponent();
 	// Setup (assumed) attached input Component
 	void SetupInputComponent();
+
+	// Figures out the player view point and reach at a given time
+	void CalculatePlayerViewpointAndReach();
 
 	// Return hit for first physics body in reach
 	FHitResult GetFirstPhysicsBodyInReach() const;
